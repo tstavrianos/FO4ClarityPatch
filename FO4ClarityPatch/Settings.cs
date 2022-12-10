6c5d9d0652368cb10d45f129914a456b772d6b65
Theodore Stavrianos <theodore@theo-s.eu>
2022-12-10 18:24:09 +0000

    first commit

---
+++ FO4ClarityPatch/Settings.cs
@@ -0,0 +1,15 @@
+﻿using Mutagen.Bethesda.WPF.Reflection.Attributes;
+
+namespace FO4ClarityPatch
+{
+    public enum RunMode
+    {
+        Clarity3,
+        Clarity4
+    }
+    public class Settings
+    {
+        [MaintainOrder] [SettingName("Version of Clarity")]
+        public RunMode Mode { get; set; } = RunMode.Clarity4;
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+Port of the Clarity Patching Script for Mod Added Interiors by WhiskyTangoFawks to Synthesis.
